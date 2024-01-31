2b326783f736881f3983df46ba6297f5cb42b040
Md MatlooB Alam <123268730+alam292@users.noreply.github.com>
2024-01-31 23:01:45 +0530

    Update reverse_number.java

--- reverse_number.java
+++ reverse_number.java
@@ -1,13 +1,15 @@
 import java.util.*;
-public class reverse_number {
+public class reverse_given_number {
     public static void main(String[]args){
-        Scanner sc  = new Scanner(System.in);
+        Scanner sc = new Scanner (System.in);
         System.out.print("Enter an original number : ");
         int number = sc.nextInt();
+        int reverse = 0;
         while(number>0){
             int lastdigit = number%10;
-            System.out.print(lastdigit);
-            number/=10;
+            reverse = (reverse*10) + lastdigit;
+            number /=10;
         }
+        System.out.println(reverse);
     }
 }
